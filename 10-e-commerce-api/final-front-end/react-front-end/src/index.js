de7057d1088950d50e3e42918b88c7d1722bdd55
MaxGitHub0515 <pavlovskymaxym4@gmail.com>
2025-03-17 14:02:48 +0100

    Uploaded second time

---
+++ 10-e-commerce-api/final-front-end/react-front-end/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import Appka from "./appc";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Appka />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
